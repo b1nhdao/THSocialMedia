5c5cc959bb440559b64a24b4ac9db27b51ff95fc
b1nhdao <binhpzo213@gmail.com>
2026-04-14 15:59:17 +0700

    add: upload files

---
+++ THSocialMedia.Application/UsecaseHandlers/Uploads/Handlers/UploadFileCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using THSocialMedia.Application.Services.StorageService;
+using THSocialMedia.Application.UsecaseHandlers.Uploads.Commands;
+
+namespace THSocialMedia.Application.UsecaseHandlers.Uploads.Handlers
+{
+    public class UploadFileCommandHandler : IRequestHandler<UploadFilesCommand, Result<List<ResponseImageModel>>>
+    {
+        private readonly IStorageService _cloudStorageService;
+
+        public UploadFileCommandHandler(IStorageService cloudStorageService)
+        {
+            _cloudStorageService = cloudStorageService;
+        }
+
+        public async Task<Result<List<ResponseImageModel>>> Handle(UploadFilesCommand request, CancellationToken cancellationToken)
+        {
+            var uploadResults = new List<ResponseImageModel>();
+            foreach (var file in request.Files)
+            {
+                var uploadResult = await _cloudStorageService.UploadFileAsync(file.OpenReadStream(), file.FileName);
+                uploadResults.Add(uploadResult);
+            }
+
+            return Result<List<ResponseImageModel>>.Success(uploadResults);
+        }
+    }
+}
---
+++ THSocialMedia.Infrastructure/Services/CloudinaryCloudStorageService.cs
@@ -0,0 +1,38 @@
+﻿using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+using THSocialMedia.Application.Services.StorageService;
+
+namespace THSocialMedia.Infrastructure.Services
+{
+    public class CloudinaryCloudStorageService : IStorageService
+    {
+        private readonly Cloudinary _cloudinary;
+        public CloudinaryCloudStorageService(Cloudinary cloudinary)
+        {
+            _cloudinary = cloudinary;
+        }
+
+        public async Task<ResponseImageModel> UploadFileAsync(Stream fileStream, string fileName)
+        {
+            var uploadParams = new RawUploadParams
+            {
+                File = new FileDescription(fileName, fileStream),
+                PublicId = $"{fileName.Replace(" ", "")}_{Guid.CreateVersion7()}",
+                Overwrite = true,
+            };
+
+            var uploadResult = await _cloudinary.UploadAsync(uploadParams);
+
+            return new ResponseImageModel
+            {
+                FileName = fileName,
+                FileUrl = uploadResult.Url.ToString()
+            };
+        }
+        public Task<string> GetUrlFile(string fileName)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
